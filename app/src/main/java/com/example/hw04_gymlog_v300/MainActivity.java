9a4c6ed758120d4e7ca2cf9a2f59c21e862b7a80
C_Fletes <166450927+Carlos-Fletes@users.noreply.github.com>
2025-04-14 19:48:58 -0700

    This is video 2 of the GymLog

--- app/src/main/java/com/example/hw04_gymlog_v300/MainActivity.java
+++ app/src/main/java/com/example/hw04_gymlog_v300/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.hw04_gymlog_v300;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +12,58 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.hw04_gymlog_v300.databinding.ActivityMainBinding;
+
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    private static final String TAG = "DAC_GYMLOG";
+    String mExercise="";
+    double mWeight=0;
+
+    int reps=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
 
-        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
+
+        binding.logDisplayTextView.setMovementMethod(new ScrollingMovementMethod());
+
+        binding.logButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getInformationFromDisplay();
+                updateDisplay();
+
+            }
+        });
+
+    }
+
+    private void updateDisplay(){
+        String currentInfo = binding.logDisplayTextView.getText().toString();
+        String newDisplay = String.format(Locale.US,"Exercise:%s%nWeight:%.2f%nReps%d%n=-=-=-=%n:", mExercise,mWeight,reps)+currentInfo;
+        binding.logDisplayTextView.setText(newDisplay);
+    }
+
+    private void getInformationFromDisplay(){
+        mExercise= binding.exerciseInputEditText.getText().toString();
+        try {
+            mWeight = Double.parseDouble(binding.weightInputEditText.getText().toString());
+        }catch(NumberFormatException e){
+            Log.d(TAG, "Error reading value from weight edit text");
+        }
+        try {
+            reps = Integer.parseInt(binding.repInputEditText.getText().toString());
+        }catch(NumberFormatException e){
+            Log.d(TAG, "Error reading value from reps edit text");
+        }
 
     }
 }
