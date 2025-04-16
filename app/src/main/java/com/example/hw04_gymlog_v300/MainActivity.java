475381dcdc88ee2fdc20090ecb3a148ca454f582
C_Fletes <166450927+Carlos-Fletes@users.noreply.github.com>
2025-04-15 19:58:19 -0700

    This is video 8 of the GymLog

--- app/src/main/java/com/example/hw04_gymlog_v300/MainActivity.java
+++ app/src/main/java/com/example/hw04_gymlog_v300/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.hw04_gymlog_v300;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
@@ -16,6 +18,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String MAIN_ACTIVITY_USER_ID = "com.example.hw04_gymlog_v300.MAIN_ACTIVITY_USER_ID";
     private GymLogRepository repository;
 
     private ActivityMainBinding binding;
@@ -34,6 +37,15 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
 
         setContentView(binding.getRoot());
+
+        loginUser();
+
+        if(loggedInUserId==-1)
+        {
+            Intent intent = LoginActivity.loginIntentFactory(getApplication());
+            startActivity(intent);
+        }
+
         repository = GymLogRepository.getRepository(getApplication());
         binding.logDisplayTextView.setMovementMethod(new ScrollingMovementMethod());
         updateDisplay();
@@ -49,6 +61,17 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    private void loginUser() {
+        loggedInUserId = getIntent().getIntExtra(MAIN_ACTIVITY_USER_ID, -1);
+    }
+
+    static Intent mainActivityIntentFactory(Context context, int userId){
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(MAIN_ACTIVITY_USER_ID, userId);
+        return intent;
+    }
+
     private void insertGymLogRecord(){
         if(mExercise.isEmpty()){
             return ;
